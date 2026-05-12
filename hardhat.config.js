ab70b8b4e3458965743f0b1474a1984368bdbd3d
zhannur17 <zannurmejrambek87@gmail.com>
2026-05-12 23:08:20 +0500

    ci: add safe Sepolia config fallbacks

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,11 +16,11 @@ module.exports = {
   },
 
   networks: {
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY],
-    },
+  sepolia: {
+    url: process.env.SEPOLIA_RPC_URL || "https://ethereum-sepolia-rpc.publicnode.com",
+    accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
   },
+},
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
   },
