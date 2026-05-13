7c2abb2f45d8428d9978300193a60f84336a6f11
zhannur17 <zannurmejrambek87@gmail.com>
2026-05-14 02:28:56 +0500

    added the subgraphs

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,12 +1,21 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
 import {
-  MarketCreated
+  MarketCreated,
 } from "../generated/MarketFactory/MarketFactory";
 
 import {
+  SharesBought,
+  MarketResolved,
+} from "../generated/templates/PredictionMarket/PredictionMarket";
+
+import { PredictionMarket } from "../generated/templates";
+
+import {
   Market,
-  Factory
+  Factory,
+  Trade,
+  Resolution,
 } from "../generated/schema";
 
 export function handleMarketCreated(event: MarketCreated): void {
@@ -20,6 +29,8 @@ export function handleMarketCreated(event: MarketCreated): void {
 
   market.save();
 
+  PredictionMarket.create(event.params.market);
+
   let factory = Factory.load("FACTORY");
 
   if (factory == null) {
@@ -32,4 +43,36 @@ export function handleMarketCreated(event: MarketCreated): void {
   );
 
   factory.save();
+}
+
+export function handleSharesBought(
+  event: SharesBought
+): void {
+  let trade = new Trade(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+
+  trade.market = event.address.toHex();
+  trade.trader = event.params.buyer;
+  trade.outcome = event.params.outcome;
+  trade.amountIn = event.params.collateralIn;
+  trade.amountOut = event.params.sharesOut;
+  trade.fee = event.params.fee;
+  trade.blockTimestamp = event.block.timestamp;
+
+  trade.save();
+}
+
+export function handleMarketResolved(
+  event: MarketResolved
+): void {
+  let resolution = new Resolution(
+    event.transaction.hash.toHex()
+  );
+
+  resolution.market = event.address.toHex();
+  resolution.winningOutcome = event.params.winningOutcome;
+  resolution.blockTimestamp = event.block.timestamp;
+
+  resolution.save();
 }
